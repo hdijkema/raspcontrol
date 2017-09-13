e27fe3d6798c0a56b5fb60cc74cc666612756b98
Hans Dijkema <hans@dykema.nl>
2017-09-13 14:28:30 +0200

    better network monitoring (32bit limits)

--- lib/network.php
+++ lib/network.php
@@ -17,35 +17,35 @@ class Network {
 
     public static function ethernet() {
         global $ssh;
-        $data = $ssh->shell_exec_noauth("/sbin/ifconfig eth0 | grep bytes");
-        #echo "<pre>$data</pre>";
-        $data = explode("\n", $data);
-        #echo "<pre>$data</pre>";
-        $rx = $data[0];
-        $tx = $data[1];
-        $lrx = preg_split("/\\s+/", $rx);
-        $ltx = preg_split("/\\s+/", $tx);
+        $dir=$_SERVER['DOCUMENT_ROOT'];
+        if (file_exists("$dir/tmp/raspcontrol_net")) {
+           $rx = file_get_contents("$dir/tmp/raspcontrol_rx");
+           $tx = file_get_contents("$dir/tmp/raspcontrol_tx");
+           $rx = $rx / 1024 / 1024;
+           $tx = $tx / 1024 / 1024;
+           $rx = round($rx, 2);
+           $tx = round($tx, 2);
+           $net = file_get_contents("$dir/tmp/raspcontrol_net");
+        } else {
+          $net = "eth0";
+          $data = $ssh->shell_exec_noauth("/sbin/ifconfig eth0 | grep bytes");
+          $data = explode("\n", $data);
+          $rx = $data[0];
+          $tx = $data[1];
+          $lrx = preg_split("/\\s+/", $rx);
+          $ltx = preg_split("/\\s+/", $tx);
 
-	#echo "<pre>$rx</pre>";
-	#echo "<pre>$tx</pre>";
-        #echo "<pre>$lrx</pre>";
-        #$lrx4 = $lrx[5];
-        #echo "<pre>$lrx4</pre>";
-       
-        #$data = str_ireplace("RX bytes:", "", $data);
-        #$data = str_ireplace("TX bytes:", "", $data);
-        #$data = trim($data);
-        #$data = explode(" ", $data);
-
-        $rxRaw = $lrx[5] / 1024 / 1024;
-        $txRaw = $ltx[5] / 1024 / 1024;
-        $rx = round($rxRaw, 2);
-        $tx = round($txRaw, 2);
+          $rxRaw = $lrx[5] / 1024 / 1024;
+          $txRaw = $ltx[5] / 1024 / 1024;
+          $rx = round($rxRaw, 2);
+          $tx = round($txRaw, 2);
+        }
 
         return array(
             'up' => $tx,
             'down' => $rx,
-            'total' => $rx + $tx
+            'total' => $rx + $tx,
+            'net' => $net
         );
     }
 
